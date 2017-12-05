fe311efb11bc67dde75bff3f5025c9a4e3068747
Odanis Tiburcio <odanistiburcio@gmail.com>
2017-12-04 23:53:42 -0400

    Agregando la clase operacion

---
+++ CGSystem/frmLogin.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CGSystem
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(12, 62);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 0;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 122);
+            this.Controls.Add(this.btnEntrar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cosobo Gym - Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+    }
+}
+
---
+++ CGSystem/operacion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina_Cia_del_Caribe
+{
+    public class operacion
+    {
+        public string Conectar()
+            {
+            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\CGSystem\\SGCSystemBD.db;Version=3;");
+                try
+                {
+                    cnx.Open();
+
+                    return "Conexión exitosa!";
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+                finally
+                {
+                    cnx.Close();
+                }
+            }
+        public string ConsultaSinResultado(string sql)
+        {
+
+            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\CGSystem\\SGCSystemBD.db;Version=3;");
+            try
+            {
+                cnx.Open();
+                SQLiteCommand command = new SQLiteCommand(sql, cnx);
+                command.ExecuteNonQuery();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                cnx.Close();
+            }
+
+        }
+
+        public DataSet ConsultaConResultado(string sql)
+        {
+
+            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\CGSystem\\SGCSystemBD.db;Version=3;");
+            try
+            {
+                cnx.Open();
+                DataSet ds = new DataSet();
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, cnx);
+                adapter.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                cnx.Close();
+            }
+
+        }
+    }
+}
