c0bb4bfcc8f6c3c8739769f2ad907ceae25026ec
Odanis Tiburcio <odanistiburcio@gmail.com>
2017-12-15 04:25:10 -0400

    Todo

--- CGSystem/Program.cs
+++ CGSystem/Program.cs
@@ -16,7 +16,7 @@ namespace CGSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ListaDeClientes());
+            Application.Run(new MenuPrincipal());
         }
     }
 }
