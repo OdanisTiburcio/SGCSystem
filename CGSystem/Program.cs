44bd0e7368dffc631d033ad539e4c6877304cf4c
Jeffry Marte Bonifacio <frijey@hotmail.com>
2018-03-08 23:16:21 -0400

    Licencia
    Funcion de Licencia con vencimientoa  los treinta días...

--- CGSystem/Program.cs
+++ CGSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,12 +12,58 @@ namespace CGSystem
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        public static bool licenciavencida = false;
+        public static operacion oper = new operacion();
+        public static bool IniciarSistema = false;
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuPrincipal());
+
+            //Confirmar Licencia de 30 días
+            DataSet ds = oper.ConsultaConResultado("SELECT * FROM licencia;");
+            bool licenciainiciada = false;
+            if (ds.Tables[0].Rows[0][0].ToString() != "PREPARADO")
+            {
+                licenciainiciada = true;
+            }else
+            {
+                licenciainiciada = false;
+            }
+
+            if (licenciainiciada)
+            {
+                DateTime hoy = DateTime.Now;
+                string fechavenci = ds.Tables[0].Rows[0][1].ToString();
+                string TreintaMasFechaInstalacion = oper.SumarAlaFecha(fechavenci, 30);
+                DateTime fechavencimiento = Convert.ToDateTime(TreintaMasFechaInstalacion);
+                if (fechavencimiento > hoy)
+                {
+                    IniciarSistema = false;
+                }
+                else
+                {
+                    IniciarSistema = true;
+                }
+            }
+            else
+            {
+
+            }
+
+            if (IniciarSistema)
+            {
+                Application.Run(new MenuPrincipal());
+            }
+            else
+            {
+
+            }
+
+            
         }
     }
 }
